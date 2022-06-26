72e332dcf65d9b7c6538f7f924aeb9704686e869
denist2322 <parksomang125@gmail.com>
2022-06-26 16:05:30 +0900

    SQL명령어 추가

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.vo.Article;
 
@@ -13,17 +15,16 @@ public interface ArticleRepository {
 	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?;
 	public Article writeArticle(String title, String body);
 	
-	// SELECT * FROM article WHERE id = ?;
-	@Select("select * from article where id = #{id}")
+	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 	
-	// DELETE FROM article WHERE id = ?;
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 	
-	// UPDATE article SET title = ?, `body` =?, updateDate = NOW(), WHERE id = ?;
+	@Update("UPDATE article SET title = #{title}, `body` = #{body} WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
-	
-	// SELECT * FROM article ORDER BY id DESC;
+		
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
 }
