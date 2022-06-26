6791b55aeaacc7d386b88e37c906b45007abddbf
denist2322 <parksomang125@gmail.com>
2022-06-26 16:54:30 +0900

    입력된 데이터만 update

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -5,22 +5,21 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-
 import com.example.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	
+
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
-	
+
 	public Article getArticle(@Param("id") int id);
-		
-	public void deleteArticle(@Param("id") int id);	
-	
+
+	public void deleteArticle(@Param("id") int id);
+
 	public void modifyArticle(int id, String title, String body);
-		
+
 	public List<Article> getArticles();
-	
+
 	public int getLastInsertId();
-	
+
 }
