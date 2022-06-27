40ba32b326fe94eafb50c15a72cd23c7248d2c27
denist2322 <parksomang125@gmail.com>
2022-06-27 11:24:00 +0900

    멤버추가

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+//	private MemberRepository memberRepository;
+		
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+//		memberReository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+	}
+
+}
