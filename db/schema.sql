7b82c0e2b63eb4fc1d3c242636796faec770b426
denist2322 <parksomang125@gmail.com>
2022-06-27 12:18:33 +0900

    회원테이블 추가

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,80 @@
-DROP DATABASE IF EXISTS sb_c_2022
-CREATE DATABASE sb_c_2022
-USE sb_c_2022
+DROP DATABASE IF EXISTS 2022_06_27;
+CREATE DATABASE 2022_06_27;
+USE 2022_06_27;
 
-# 게시물 생성
 CREATE TABLE article(
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
     title CHAR(100) NOT NULL,
-    `body` TEXT NOT NULL 
+    `body` TEXT NOT NULL
 );
 
-# 게시물, 테스트 데이터 생성
-INSERT INTO article
-SET regDate = NOW(),
+INSERT INTO article SET 
+regDate = NOW(),
 updateDate = NOW(),
 title = "제목1",
 `body` = "내용1";
 
-INSERT INTO article
-SET regDate = NOW(),
+INSERT INTO article SET 
+regDate = NOW(),
 updateDate = NOW(),
 title = "제목2",
 `body` = "내용2";
 
-INSERT INTO article
-SET regDate = NOW(),
+INSERT INTO article SET 
+regDate = NOW(),
 updateDate = NOW(),
 title = "제목3",
 `body` = "내용3";
 
+#회원 테이블
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(60) NOT NULL,
+    authLevel SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨(3=일반,7=관리자)',
+    `name` CHAR(20) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    cellphoneNo CHAR(20) NOT NULL,
+    email CHAR(50) NOT NULL,
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전,1=탈퇴)',
+    delDate DATETIME COMMENT '탈퇴날짜'
+);
+
+# 회원 ,테스트 데이터
+INSERT INTO `member` SET 
+    regDate = NOW(),
+    updateDate = NOW(),
+    loginId = 'admin',
+    loginPw = 'admin',
+    authLevel = 7,
+    `name` = '관리자',
+    nickname = '관리자',
+    cellphoneNo = '01000000000',
+    email = 'dsdss@naver.com';
+ 
+INSERT INTO `member` SET 
+    regDate = NOW(),
+    updateDate = NOW(),
+    loginId = 'user1',
+    loginPw = 'user1',
+    `name` = '사용자1',
+    nickname = '사용자1',
+    cellphoneNo = '01000000000',
+    email = 'dsdss@naver.com';
+    
+INSERT INTO `member` SET 
+    regDate = NOW(),
+    updateDate = NOW(),
+    loginId = 'user2',
+    loginPw = 'user2',
+    `name` = '사용자2',
+    nickname = '사용자2',
+    cellphoneNo = '01000000000',
+    email = 'dsdss@naver.com';
+
+SELECT * FROM `member`;
+ 
